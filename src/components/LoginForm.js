848a7b557545a6aa4eaea6e6e9e8929158667386
DeniseFafette <denisefafette@gmail.com>
2019-11-14 17:38:20 -0800

    debug yup

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,56 +3,72 @@ import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-const LoginForm = ({values, errors, touched, status }) => {
-    const [users, setUsers] = useState([]);
-}
 
-function LoginForm() {
+function LoginForm({values, errors, touched, status}) {
+    const [users, setUsers] = useState([]);
     return (
     <div className='user-form'>
-        <form>
-            <input
+        <Form>
+            <Field
                 type="text"
                 name="name"
                 placeholder="Name"
             />
-            <input
+            {touched.name && errors.name && (
+                <p className="errors">{errors.name}</p>
+            )}
+            <Field
                 type="text"
                 name="email"
                 placeholder="Email"
             />
-             <input
+            {touched.email && errors.email && (
+                <p className="errors">{errors.email}</p>
+            )}
+             <Field
                 type="text"
                 name="password"
                 placeholder="Password"
             />
-            <p>Terms of Service:</p>
+            {touched.password && errors.password && (
+                <p className="errors">{errors.password}</p>
+            )}
+            <label> 
+                Terms of Service
+                <Field
+                type="checkbox"
+                name="terms"
+                checked={values.terms} 
+            />
+            </label>
             <button>Submit</button>
-        </form>
+        </Form>
     </div>
     );
 };
 
 const FormikLoginForm = withFormik({
-    mapPropsToValues({name, email, password}){
+    mapPropsToValues({name, email, password, terms}){
         return {
             name: name || "",
             email: email || "",
             password: password || "",
+            terms: terms || false,
         };
     },
     validationSchema: Yup.object().shape({
-        name: Yup.string().required(),
-        email: Yup.string().required(),
-        password: Yup.string().required(),
+        name: Yup.string().required('Name is required'),
+        email: Yup.string().required('Email is required'),
+        password: Yup.string().required('Password is required'),
     }),
     handleSubmit(values, { setStatus }) {
         axios
-        .post(, values)
+        .post("https://reqres.in/api/users", values)
         .then(res => {
-            setStatus(res.data);
+            setStatus(res.users);
             console.log(res);
         })
+        .catch(err => console.group(err.response));
     }
 })(LoginForm)
 
